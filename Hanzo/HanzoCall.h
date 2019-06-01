0b70f5e37a7516adddaa2349eec0777b3fcff387
emacser <roxman98@naver.com>
2019-06-01 17:30:35 +0100

    .

--- Hanzo/HanzoCall.h
+++
@@ -1,5 +0,0 @@
-#ifndef HANZO_CALL_H
-#define HANZO_CALL_H
-#define HanzoCall(class, function, ...) (class.function(&class, __VA_ARGS__))
-#define HanzoCallP(class, function, ...) (class->function(class, __VA_ARGS__))
-#endif
